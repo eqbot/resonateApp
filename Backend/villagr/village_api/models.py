f682b3eb393cda1521ac5ab914a87a54b60a2513
eqbot <trentkenn8@gmail.com>
2020-03-18 19:20:23 -0700

    Add docstrings and fix other linting issues in models

--- Backend/villagr/village_api/models.py
+++ Backend/villagr/village_api/models.py
@@ -3,21 +3,39 @@ from phone_field import PhoneField
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
 class Person(models.Model):
+    """
+    Information on a village attendee
+    """
     phone = PhoneField(primary_key=True)
     name = models.CharField(max_length=128)
     discipled_by = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
 
 class Village(models.Model):
+    """
+    A village
+    """
     description = models.CharField(max_length=128)
 
 class UserProfile(AbstractUser):
-    leads_village = models.ForeignKey(Village, on_delete=models.SET_NULL, null=True, related_name='leaders')
+    """
+    Application user data
+    """
+    leads_village = models.ForeignKey(
+        Village,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='leaders'
+    )
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
 
 class AttendanceLog(models.Model):
+    """
+    Village attendance log
+    """
     WEEKLYMEETING = 'WM'
     OTHER = 'OT'
     MEETING_TYPES = [
